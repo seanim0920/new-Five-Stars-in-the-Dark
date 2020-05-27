b85d5394b2864feda353a37c8dceb1b4ea7826b6
sean <seanim0920@gmail.com>
2020-05-27 10:49:55 -0700

    Added files for tutorial level

---
+++ Five Stars in the Dark/Assets/Scripts/Level/Obstacles/TutorialCurbFailure.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialCurbFailure : ObstacleFailure
+{
+    [SerializeField]
+    private GameObject curbTableaux;
+    private static int numCurbCrashes = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        failureDialogues = Resources.LoadAll<AudioClip>(SceneManager.GetActiveScene().name + "/Failure/ScriptedCurb");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void playFailure(Collision2D col)
+    {
+        if(numCurbCrashes - 1 < failureDialogues.Length)
+        {
+            Debug.Log("error made playing numCurbCrashes - 1: " + (numCurbCrashes - 1));
+            StartCoroutine(PlayError.PauseDialogueCoroutine(failureDialogues[numCurbCrashes - 1]));
+        }
+        else
+        {
+            failureDialogues = Resources.LoadAll<AudioClip>(SceneManager.GetActiveScene().name + "/Failure/GenericCrash");
+
+            System.Random rand = new System.Random();
+            numDialogue = rand.Next(0, failureDialogues.Length);
+            Debug.Log("error made playing numDialogue: " + numDialogue);
+            StartCoroutine(PlayError.PauseDialogueCoroutine(failureDialogues[numDialogue]));
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.CompareTag("Player"))
+        {
+            numCurbCrashes++;
+            if(numCurbCrashes == 1)
+            {
+                var tableaux = Instantiate(curbTableaux);
+                tableaux.GetComponent<DisplayStrafeTableaux>().tableauxNum = 2;
+            }
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if(col.gameObject.CompareTag("Player") && numCurbCrashes <= 2)
+        {
+            playFailure(col);
+        }
+    }
+}
