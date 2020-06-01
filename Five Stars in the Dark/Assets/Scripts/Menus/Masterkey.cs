ec8beb2c5285517f68703874f674fa605962993b
Charles Advincula <chadvincula7636428@gmail.com>
2020-06-01 03:19:40 -0700

    Play button from instructions screen now leads to the tutorial level

--- Five Stars in the Dark/Assets/Scripts/Menus/Masterkey.cs
+++ Five Stars in the Dark/Assets/Scripts/Menus/Masterkey.cs
@@ -13,12 +13,13 @@ public class Masterkey : MonoBehaviour
     public Button levelBack;
     public static bool egg = false;
     public static bool lvl = false;
-    public static string sceneName = "Level 1";
+    public static string sceneName = "Tutorial";
     public Button tutorial;
     public Button level1;
     public Button level2;
     public Button level3;
     public Button level4;
+    public Button level5;
 
 	public static bool played = false;
 
@@ -38,10 +39,11 @@ public class Masterkey : MonoBehaviour
         levelBack.onClick.AddListener(TaskTitle);
 
         tutorial.onClick.AddListener(() => sceneName = "Tutorial");
-        level1.onClick.AddListener(() => sceneName = "Tutorial");
+        level1.onClick.AddListener(() => sceneName = "Level 1");
         level2.onClick.AddListener(() => sceneName = "Level 2");
         level3.onClick.AddListener(() => sceneName = "Level 3");
         level4.onClick.AddListener(() => sceneName = "Level 4");
+        level5.onClick.AddListener(() => sceneName = "Level 5");
     }
 
     // Update is called once per frame
@@ -65,6 +67,7 @@ public class Masterkey : MonoBehaviour
 
     public void TaskTitle()
     {
+        egg = false;
         lvl = false;
         EventSystem.current.SetSelectedGameObject(null);
         EventSystem.current.SetSelectedGameObject(start.gameObject);
