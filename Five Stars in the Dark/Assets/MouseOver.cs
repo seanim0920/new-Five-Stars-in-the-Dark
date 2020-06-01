5777b834876878f3fcf5a874f8301d8292fa7aa9
sean <seanim0920@gmail.com>
2020-05-31 21:43:25 -0700

    MasterKey refactored Buttons prefabbed Only one button per screen can be highlighted now Spam-clicks don't interrupt transitions Animation made for main menu buttons, though it's unused Sfx for hovering Bug fix - hovering sfx does not play during transitions MouseOver.cs and event trigger included in menubutton prefab to handle events Settings panel updated with button prefabs "Selected" tag included to indicate the default button that is selected for a screen - Currently the back button for each screen is this

---
+++ Five Stars in the Dark/Assets/MouseOver.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseOver : MonoBehaviour
+{
+    public AudioSource audio;
+    public Animator anims;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anims = GetComponent<Animator>();
+    }
+
+    public void OnPointerEnterDelegate()
+    {
+        EventSystem.current.SetSelectedGameObject(gameObject);
+    }
+
+    public void OnSelectDelegate()
+    {
+        anims.CrossFade("MenuButton", 0.3f);
+        audio.Play();
+    }
+
+    public void OnDeselectDelegate()
+    {
+        anims.CrossFade("Start", 0.3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
