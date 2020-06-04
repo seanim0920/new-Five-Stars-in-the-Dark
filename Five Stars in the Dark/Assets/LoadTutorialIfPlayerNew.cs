2a6ec04e766b5a2b3d6041b1686daf9bdd24f8ce
sean <seanim0920@gmail.com>
2020-06-04 01:52:34 -0700

    Debounced buttons that load new scenes Stereo Test screen takes you to the tutorial if player hasn't played it yet

---
+++ Five Stars in the Dark/Assets/LoadTutorialIfPlayerNew.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadTutorialIfPlayerNew : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() => {
+            if (PlaythroughManager.hasPlayedLevel(PlaythroughManager.currentLevelIndex + 1))
+            {
+                transform.parent.parent.GetComponent<LoadScene>().LoadLevelFromMenu("Menu");
+            } else
+            {
+                transform.parent.parent.GetComponent<LoadScene>().LoadLevelFromMenu("Tutorial");
+            }
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
