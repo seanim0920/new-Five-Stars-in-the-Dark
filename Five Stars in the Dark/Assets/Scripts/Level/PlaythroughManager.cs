4d41aa4e794f9b7ce29105bf0b78f197fb2faa93
Charles Advincula <chadvincula7636428@gmail.com>
2020-06-04 15:44:47 -0700

    I believe play history persists accross multiple play sessions now

--- Five Stars in the Dark/Assets/Scripts/Level/PlaythroughManager.cs
+++ Five Stars in the Dark/Assets/Scripts/Level/PlaythroughManager.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public static class PlaythroughManager
@@ -9,6 +11,59 @@ public static class PlaythroughManager
 
     public static bool hasPlayedLevel(int levelBuildIndex)
     {
+        string path = Application.persistentDataPath + "/PlayHistory.txt";
+        Debug.Log("Playthrough Manager");
+        Debug.Log("Path: " + path);
+        // Path doesn't exist => first play session
+        if(!File.Exists(path))
+        {
+            return false;
+        }
+
+        // Else read from file and copy contents into temp list
+        string textLevels = File.ReadAllText(path);
+        Debug.Log("levels as text: " + textLevels);
+        string[] levels = textLevels.Split('-');
+        Debug.Log("num levels played: " + levels.Length);
+        List<int> tempList = new List<int>();
+        for(int i = 0; i < levels.Length; i++)
+        {
+            if(string.Equals(levels[i], "-") || string.Equals(levels[i], " ") || string.Equals(levels[i], ""))
+            {
+                continue;
+            }
+            tempList.Add(int.Parse(levels[i]));
+        }
+
+        tempList.ForEach(Print);
+
+        // Set actual list to temp list to prevent adding multiple of the same level
+        // when checking for play history
+        playedLevels = tempList;
+        Debug.Log("Played this level? " + (playedLevels.Contains(levelBuildIndex) ? "y" : "n"));
         return playedLevels.Contains(levelBuildIndex);
+    }
+
+    // Check if player hasPlayedLevel before saving level history
+    public static void saveLevelHistory(int levelBuildIndex)
+    {
+        playedLevels.Add(levelBuildIndex);
+
+        string path = Application.persistentDataPath + "/PlayHistory.txt";
+        Debug.Log("Playthrough Manager");
+        Debug.Log("Path: " + path);
+        string textLevels = "";
+        foreach (int level in playedLevels)
+        {
+            textLevels += level + "-";
+        }
+        Debug.Log("levels as text: " + textLevels);
+        File.WriteAllText(path, textLevels);
+        Debug.Log("Played this level? " + (playedLevels.Contains(levelBuildIndex) ? "y" : "n"));
+    }
+
+    private static void Print(int n)
+    {
+        Debug.Log("Played Level " + n);
     }
 }
