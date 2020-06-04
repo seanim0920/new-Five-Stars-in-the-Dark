1d47d4489d919dd3ecf74e43090138b6dc7b38e8
Brian_Chu <bcchu@ucsc.edu>
2020-06-03 18:29:15 -0700

    final credits sequence assuming resolution didnt mess anything up

---
+++ Five Stars in the Dark/Assets/Scripts/UI/ReturnMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ReturnMenu : MonoBehaviour
+{
+	public Button skip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        skip.onClick.AddListener(TaskSkip);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = transform.position;
+		if(transform.position.x <= -3000) {
+			SceneManager.LoadScene("Menu");
+			
+		}
+    }
+	
+	void TaskSkip(){
+		SceneManager.LoadScene("Menu");
+	}
+}
