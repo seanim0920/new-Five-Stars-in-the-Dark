1ddd09c96e63b490a94964d3e1bd60da93d6d072
sean <seanim0920@gmail.com>
2020-06-04 13:32:57 -0700

    Fixed navigation for end and fail screens SelectButtonOnStart mutes the button at first

--- Five Stars in the Dark/Assets/SelectButtonOnStart.cs
+++ Five Stars in the Dark/Assets/SelectButtonOnStart.cs
@@ -8,6 +8,7 @@ public class SelectButtonOnStart : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        GetComponent<AudioSource>().mute = true;
         EventSystem.current.SetSelectedGameObject(gameObject);
     }
 
