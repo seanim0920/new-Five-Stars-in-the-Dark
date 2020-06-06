771402882f723fbb7f828075b016d8a6dfe73bfa
Charles Advincula <chadvincula7636428@gmail.com>
2020-06-06 01:23:29 -0700

    When behind the player, police now wait... then warn thep player... then proceed going at full speed

---
+++ Five Stars in the Dark/Assets/Scripts/Level/Obstacles/Cars/GenerateRandomPullOver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateRandomPullOver : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource honk;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioClip[] failureDialogues = Resources.LoadAll<AudioClip>("Audio/Police Chase");
+        System.Random rand = new System.Random();
+        int numDialogue = rand.Next(0, failureDialogues.Length);
+        if (failureDialogues.Length > 0)
+        {
+            honk.clip = failureDialogues[numDialogue];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
