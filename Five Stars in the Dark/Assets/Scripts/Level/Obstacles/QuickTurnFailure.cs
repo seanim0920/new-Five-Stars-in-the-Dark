dd600f5e01f1f685da0084ba5c5aa9f1b7484303
Charles Advincula <chadvincula7636428@gmail.com>
2020-06-09 01:49:19 -0700

    Quick turns now reference the correct folder to get failure dialogue

--- Five Stars in the Dark/Assets/Scripts/Level/Obstacles/QuickTurnFailure.cs
+++ Five Stars in the Dark/Assets/Scripts/Level/Obstacles/QuickTurnFailure.cs
@@ -5,11 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class QuickTurnFailure : ObstacleFailure
 {
+    private static int numFaliures = 0;
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
-        failureDialogues = Resources.LoadAll<AudioClip>(SceneManager.GetActiveScene().name + "/Failure/QuickTurn");
+        failureDialogues = Resources.LoadAll<AudioClip>("LevelFiles/" + SceneManager.GetActiveScene().name + "/Failure/QuickTurn");
+        // numFaliures = 0;
     }
 
     // Update is called once per frame
@@ -21,11 +23,16 @@ public class QuickTurnFailure : ObstacleFailure
     public override void playFailure(Vector3 point)
     {
         // Play failure dialogue sequentially
-        if(numDialogue < failureDialogues.Length)
+        if(numFaliures >= failureDialogues.Length)
         {
-            dialogueSource.clip = failureDialogues[numDialogue];
-            numDialogue++;
+            // dialogueSource.clip = failureDialogues[numFaliures];
+            // numFaliures++;
+            numFaliures = 0;
         }
+        Debug.Log("What # dialogue? " + numFaliures);
+        // StartCoroutine(PlayError.PlayWarningClipCoroutine(failureDialogues[numFaliures]));
+        dialogueSource.clip = failureDialogues[numFaliures];
         base.playFailure(point);
+        numFaliures++;
     }
 }
