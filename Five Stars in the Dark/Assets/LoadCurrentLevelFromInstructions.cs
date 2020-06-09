d1336d23d255c947671661ab12f8edadb3b0f2dc
sean <seanim0920@gmail.com>
2020-06-09 01:39:20 -0700

    Start button in instructions screen will now go to the last scene the player has played.

---
+++ Five Stars in the Dark/Assets/LoadCurrentLevelFromInstructions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LoadCurrentLevelFromInstructions : MonoBehaviour
+{
+    public LoadScene localLoadScene;
+    int beginningIndex = 3;
+    int endIndex = 13;
+    int sceneToLoad = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (sceneToLoad = beginningIndex; sceneToLoad <= endIndex; ++sceneToLoad)
+        {
+            if (!PlaythroughManager.hasPlayedLevel(sceneToLoad))
+            {
+                --sceneToLoad;
+                if (sceneToLoad <= 3) sceneToLoad = 3;
+                break;
+            }
+        }
+
+        Debug.Log("start will load scene " + sceneToLoad);
+
+        GetComponent<Button>().onClick.AddListener(() => {
+            localLoadScene.LoadLevelFromMenu(sceneToLoad);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
