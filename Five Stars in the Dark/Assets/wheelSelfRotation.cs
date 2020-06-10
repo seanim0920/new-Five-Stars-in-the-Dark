e59cfe3ed759b7e7ea4dcc07f1fd0067fadb1487
sean <seanim0920@gmail.com>
2020-06-09 17:30:41 -0700

    Level 5 checkengine asset implemented, and included the files for the level ending

---
+++ Five Stars in the Dark/Assets/wheelSelfRotation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wheelSelfRotation : MonoBehaviour
+{
+    public RectTransform wheelImage;
+    private float wheelAngle;
+    private float speed;
+    private float goalAngle;
+    private bool right;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resetGoalAngle();
+        changeWheelImageAngle();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        changeWheelImageAngle();
+        wheelImage.rotation = Quaternion.Euler(0, 0, wheelAngle);
+    }
+    public void changeWheelImageAngle()
+    {
+        wheelAngle += speed;
+        if ((right && wheelAngle >= goalAngle) || (!right && wheelAngle <= goalAngle))
+        {
+            resetGoalAngle();
+        }
+    }
+
+    private void resetGoalAngle()
+    {
+        goalAngle = Random.Range(-10.00f, 45.00f);
+        speed = Random.Range(0f, 0.4f);
+        right = true;
+        if (goalAngle < wheelAngle)
+        {
+            speed *= -1;
+            right = false;
+        }
+    }
+}
