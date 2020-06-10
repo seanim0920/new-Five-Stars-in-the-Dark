707ec6941c3c4e511b9a6b946da85bcfd3ecda7f
sean <seanim0920@gmail.com>
2020-06-09 17:46:23 -0700

    Policecar's siren sound changed for level 5

---
+++ Five Stars in the Dark/Assets/Level5PoliceVariant.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level5PoliceVariant : MonoBehaviour
+{
+    public AudioSource siren;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "Level 5")
+        {
+            siren.clip = Resources.Load<AudioClip>("Audio/drone/MissileWhistle");
+            siren.dopplerLevel = 3;
+            siren.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
