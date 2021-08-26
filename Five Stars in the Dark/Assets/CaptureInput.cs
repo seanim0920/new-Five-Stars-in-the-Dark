bede284c61f14472df9c5b379f88e4b83aa874af
sean <seanim0920@gmail.com>
2021-08-26 02:01:36 -0700

    will allow all axes to be toggleable using the settings menu. will also plan to combine the steering wheel, keyboard and gamepad scripts into one so the player wont have to worry about switching between all three. dunno if we'll have time for that since that may involve tweaking the controls. we'll see.

---
+++ Five Stars in the Dark/Assets/CaptureInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CaptureInput : MonoBehaviour
+{
+    static readonly KeyCode[] _keyCodes =
+        System.Enum.GetValues(typeof(KeyCode))
+            .Cast<KeyCode>()
+            .Where(k => ((int)k < (int)KeyCode.Mouse0)) //should work with controller button presses too
+            .ToArray();
+
+    private bool isChecking = false;
+    //public InputKey input; //
+
+    public void setChecking()
+    {
+        isChecking = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isChecking && Input.anyKeyDown)
+            for (int i = 0; i < _keyCodes.Length; i++)
+                if (Input.GetKeyDown(_keyCodes[i])) { }
+                    //input = _keyCodes[i];
+    }
+}
